baff7ae5219b2bf59133f8c9057e48704170a106
Jeroen Bellen <bellenjeroen@gmail.com>
2017-01-17 20:07:06 +0100

    Adds event

---
+++ app/eventcore/event.js
@@ -0,0 +1,37 @@
+"use strict"; 
+
+const p = new WeakMap();
+
+class Event {
+	
+	constructor(aggregate, when, name, data) {
+		p.set(this, {
+			aggregate: aggregate,
+			when: when,
+			name: name,
+			data: data
+		});
+	}
+
+	get aggregate() {
+		return p.get(this).aggregate;
+	}
+
+	get when() {
+		return p.get(this).when;
+	}
+
+	get name() {
+		return p.get(this).name;
+	}
+
+	get data() {
+		return p.get(this).data;
+	}
+
+	toString() {
+		return `Event aggregate=[${this.aggregate}] when=[${this.when}] name=[${this.name}]`
+	}
+}
+
+module.exports = Event;
---
+++ test/eventcore/eventTest.js
@@ -0,0 +1,17 @@
+const expect = require("chai").expect;
+const Event = require("../../app/eventcore/event");
+
+describe("Event Test", () => {
+	const event = new Event("agg", 123, "name", {data: "data"});
+	
+	it("gets aggregate", () => expect(event.aggregate).to.equal("agg"));
+
+	it("gets when", () => expect(event.when).to.equal(123));
+
+	it("gets name", () => expect(event.name).to.equal("name"));
+
+	it("gets data", () => expect(event.data).to.deep.equal({data: "data"}));
+
+	it("toString", () => expect(event.toString()).to.equal("Event aggregate=[agg] when=[123] name=[name]"));
+
+});
